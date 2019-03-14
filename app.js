a8a6ebdcc41ea3fd731526ff8c0a05c10126d9f4
Cristian Felipe Buitrago Rodriguez <36422189+buty619@users.noreply.github.com>
2019-03-14 15:00:54 -0500

    push

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.get('/', (req, res) => {
   res.send("<form action=\"/hello\" method=\"post\"><input type=\"text\" id=\"name\" name=\"name\"><button type=\"submit\">Enviar</button></form>");  
 });
 app.post("/hello", (req,res) =>{
-  res.send("<h1> Hola "+req.body.name+"!</h1>");  
+  res.send("<h1>Hola "+req.body.name+"!</h1>");  
 })
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
