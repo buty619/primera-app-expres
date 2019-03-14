f125860638596550f59fc06792a6ebb5e11b5b32
Cristian Felipe Buitrago Rodriguez <36422189+buty619@users.noreply.github.com>
2019-03-14 12:12:31 -0500

    push

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 
 
 app.get('/', (req, res) => {
+  var query = require('url').parse(req.url,true).query;
   let name = req.query.name;
   res.send('<h1>Hola '+ name +'!</h1>');
 });
