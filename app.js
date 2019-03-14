ebd53be121e4f6a4f367dfcd0d63039e02498946
Cristian Felipe Buitrago Rodriguez <36422189+buty619@users.noreply.github.com>
2019-03-14 16:10:58 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,12 +2,11 @@ const express = require('express');
 const app = express();
 
 
+
 app.use(express.urlencoded());
-app.get('/', (req, res) => {
-  res.send("<form action=\"/hello\" method=\"post\"><input type=\"text\" id=\"name\" name=\"name\"><button type=\"submit\">Enviar</button></form>");  
+app.get('/', function(req, res) {  
+    console.log(req.headers);    
+    res.send(req.headers['user-agent'])
 });
-app.post("/hello", (req,res) =>{
-  res.send("<h1>Hola "+req.body.name+"!</h1>");  
-})
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
