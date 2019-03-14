06b514775ca2589bbe38b62f40e33a495c367d6e
Cristian Felipe Buitrago Rodriguez <36422189+buty619@users.noreply.github.com>
2019-03-14 14:57:26 -0500

    push

--- app.js
+++ app.js
@@ -2,17 +2,12 @@ const express = require('express');
 const app = express();
 
 
-
+app.use(express.urlencoded());
 app.get('/', (req, res) => {
-  out="";
-  for (let i = 1; i < 51; i++) {
-    if(i % 2 === 0){
-      out = out + '<p>'+i+' Soy Par!</p>';
-    }else{
-      out = out +'<p>'+i+' Soy Impar!</p>';
-    }    
-  }
-  res.send(out);  
+  res.send("<form action=\"/hello\" method=\"post\"><input type=\"text\" id=\"name\" name=\"name\"><button type=\"submit\">Enviar</button></form>");  
 });
+app.post("/hello", (req,res) =>{
+  res.send("<h1>"+req.body.name+"</h1>");  
+})
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
